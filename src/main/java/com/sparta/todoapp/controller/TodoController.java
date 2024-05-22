6c3fcc06e13cd21358df0a4e5de1be94cfb50e4d
User <sonael121@naver.com>
2024-05-22 19:36:05 +0900

    추가구현 기능 응담에 스테이터스 코드 및 메세지 추가

--- src/main/java/com/sparta/todoapp/controller/TodoController.java
+++ src/main/java/com/sparta/todoapp/controller/TodoController.java
@@ -1,11 +1,13 @@
 package com.sparta.todoapp.controller;
 
+import com.sparta.todoapp.CommonResponse;
 import com.sparta.todoapp.repository.Todo;
 import com.sparta.todoapp.repository.TodoRepository;
 import com.sparta.todoapp.service.TodoService;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,38 +28,57 @@ public class TodoController {
 
 
     @PostMapping
-    public ResponseEntity<TodoResponseDTO> postTodo(@RequestBody TodoRequestDTO dto){
+    public ResponseEntity<CommonResponse<TodoResponseDTO>> postTodo(@RequestBody TodoRequestDTO dto){
         Todo  todo = todoService.createTodo(dto);
         TodoResponseDTO response = new TodoResponseDTO(todo);
-        return ResponseEntity.ok().body(response);
+        return ResponseEntity.ok().body(CommonResponse.<TodoResponseDTO>builder()
+            .statusCode(HttpStatus.OK.value())
+            .msg("생성완료됨")
+            .data(response)
+            .build());
     }
 
     @GetMapping("/{todoId}")
-    public ResponseEntity<TodoResponseDTO> getTodo(@PathVariable Long todoId){
+    public ResponseEntity<CommonResponse<TodoResponseDTO>> getTodo(@PathVariable Long todoId){
         Todo todo = todoService.getTodo(todoId);
         TodoResponseDTO response = new TodoResponseDTO(todo);
-        return ResponseEntity.ok().body(response);
+        return ResponseEntity.ok().body(CommonResponse.<TodoResponseDTO>builder()
+            .statusCode(HttpStatus.OK.value())
+            .msg("조회완료됨")
+            .data(response)
+            .build());
     }
 
     @GetMapping
-    public ResponseEntity<List<TodoResponseDTO>> getTodos() {
+    public ResponseEntity<CommonResponse<List<TodoResponseDTO>>> getTodos() {
         List<Todo> todos = todoService.getTodos();
         List<TodoResponseDTO> response = todos.stream().map(TodoResponseDTO::new).collect(
             Collectors.toList());
-        return ResponseEntity.ok().body(response);
+        return ResponseEntity.ok().body(CommonResponse.<List<TodoResponseDTO>>builder()
+            .statusCode(HttpStatus.OK.value())
+            .msg("목록조회완료됨")
+            .data(response)
+            .build());
     }
 
     @PutMapping("/{todoId}")
-    public ResponseEntity<TodoResponseDTO> putTodo(@PathVariable Long todoId, @RequestBody TodoRequestDTO dto){
+    public ResponseEntity<CommonResponse<TodoResponseDTO>> putTodo(@PathVariable Long todoId, @RequestBody TodoRequestDTO dto){
         Todo todo = todoService.updateTodo(todoId, dto);
         TodoResponseDTO response = new TodoResponseDTO(todo);
-        return ResponseEntity.ok().body(response);
+        return ResponseEntity.ok().body(CommonResponse.<TodoResponseDTO>builder()
+            .statusCode(HttpStatus.OK.value())
+            .msg("수정완료됨")
+            .data(response)
+            .build());
     }
 
 
     @DeleteMapping("/{todoId}")
-    public ResponseEntity<Void> deleteTodo(@PathVariable Long todoId, @RequestBody TodoRequestDTO dto) {
+    public ResponseEntity<CommonResponse> deleteTodo(@PathVariable Long todoId, @RequestBody TodoRequestDTO dto) {
         todoService.deleteTodo(todoId, dto.getPassword());
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok().body(CommonResponse.builder()
+            .statusCode(HttpStatus.OK.value())
+            .msg("삭제완료됨")
+            .build());
     }
 }
